b7e3e7342a4264396a36e2472442f0fabe0c80ab
sendyakbar <msendyakbars@gmail.com>
2023-08-10 16:18:01 +0700

    refactor: auto capitalize input to none

--- src/base/Input.tsx
+++ src/base/Input.tsx
@@ -17,6 +17,7 @@ export default function Input(props: Props): JSX.Element {
         onChangeText={onChangeText}
         value={value}
         style={styles.textInput}
+        autoCapitalize="none"
       />
     </View>
   );
