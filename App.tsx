ddcd1393a44ddac39b3d85aae92ea826523dfed0
sendyakbar <msendyakbars@gmail.com>
2023-08-10 14:14:10 +0700

    feature: initial setup recoil

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {RecoilRoot} from 'recoil';
 
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
