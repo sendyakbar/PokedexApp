19d5cf489ef2e550ebc4c1b4c803cc655f83becd
sendyakbar <msendyakbars@gmail.com>
2023-08-11 08:06:04 +0700

    feature: create UseHelpers hook

---
+++ src/hooks/UseHelpers.tsx
@@ -0,0 +1,36 @@
+import {useCallback} from 'react';
+
+type ItemTypesType = {
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+type ItemMovesType = {
+  move: {
+    name: string;
+    url: string;
+  };
+};
+
+const useHelpers = () => {
+  const typesFormatter = useCallback((types: ItemTypesType[]) => {
+    const remapType: string[] = types.map(
+      (item: ItemTypesType) => item.type.name,
+    );
+    return remapType.join(', ');
+  }, []);
+
+  const movesFormatter = useCallback((moves: ItemMovesType[]) => {
+    const remapMoves: string[] = moves.map(
+      (item: ItemMovesType) => item.move.name,
+    );
+    const slicedMoves = remapMoves.slice(0, 10);
+    return slicedMoves.join(', ');
+  }, []);
+
+  return {typesFormatter, movesFormatter};
+};
+
+export default useHelpers;
