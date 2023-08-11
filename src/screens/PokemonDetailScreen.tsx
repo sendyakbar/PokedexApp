ff5963bc376b57f1bfde80d4c813392a33845c81
sendyakbar <msendyakbars@gmail.com>
2023-08-11 08:06:22 +0700

    feature: pokemon detail screen

--- src/screens/PokemonDetailScreen.tsx
+++ src/screens/PokemonDetailScreen.tsx
@@ -1,8 +1,14 @@
-import React, {useLayoutEffect} from 'react';
-import {StyleSheet, View, Text, ViewStyle} from 'react-native';
+import React, {useLayoutEffect, useEffect, useState} from 'react';
+import {StyleSheet, ViewStyle, ScrollView} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {RootStackParamList} from '../navigation/RootNavigator';
+import Loading from '../base/Loading';
+import useDataFetcher from '../hooks/UseDataFetcher';
+import useHelpers from '../hooks/UseHelpers';
+import useFetchEvoChain from '../hooks/UseFetchEvoChain';
+import DetailBoxComponent from '../components/DetailBoxComponent';
+import SpritesComponent from '../components/SpritesComponent';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'PokemonDetailScreen'>;
 
@@ -10,7 +16,11 @@ export default function PokemonDetailScreen({
   navigation,
   route,
 }: Props): JSX.Element {
-  const {name} = route.params;
+  const {name, url} = route.params;
+  const {isLoading, response} = useDataFetcher(url);
+  const {typesFormatter, movesFormatter} = useHelpers();
+  const {formattedEvoChain} = useFetchEvoChain(response.id);
+  const [pics, setPics] = useState<string[]>([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -18,10 +28,33 @@ export default function PokemonDetailScreen({
     });
   }, [navigation, name]);
 
+  useEffect(() => {
+    if (response.sprites) {
+      const spritesList: string[] = Object.values(response.sprites);
+      const filterSprites: string[] = spritesList.filter(
+        (uri: unknown) => typeof uri === 'string',
+      );
+      setPics(filterSprites);
+    }
+  }, [response]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Halo</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <SpritesComponent label="Sprites" pics={pics} />
+      <DetailBoxComponent
+        data={[
+          {label: 'Height', value: response.height},
+          {label: 'Weight', value: response.weight},
+          {label: 'Types', value: typesFormatter(response.types)},
+          {label: 'Evo Chain', value: formattedEvoChain},
+          {label: 'Moves', value: movesFormatter(response.moves)},
+        ]}
+      />
+    </ScrollView>
   );
 }
 
